57927aae3a911932b14ca61f84fbe4349206cd81
vipulpun <pm.vipul@gmail.com>
2024-06-05 22:49:19 +0545

    Add project files.

---
+++ WeatherForecastApplication/Controllers/WeatherController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForecastApplication.Models;
+using Newtonsoft.Json;
+
+namespace WeatherForecastApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public WeatherController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string city)
+        {
+            if (string.IsNullOrEmpty(city))
+            {
+                ModelState.AddModelError(string.Empty, "Please enter a city name.");
+                return View();
+            }
+
+            string apiKey = "ca0b7d7c84f31afbd4050ecac4490e77";
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+                return View(weatherData);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Could not retrieve weather data. Please try again.");
+                return View();
+            }
+        }
+    }
+}
---
+++ WeatherForecastApplication/Models/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherForecastApplication.Models
+{
+    public class WeatherData
+    {
+        public Main Main { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+}
