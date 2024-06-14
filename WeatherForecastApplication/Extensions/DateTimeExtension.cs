0da0b916bc04e5e8fd9183a303276783f9a0eabf
vipulpun <pm.vipul@gmail.com>
2024-06-14 17:05:09 +0545

    Changes

---
+++ WeatherForecastApplication/Extensions/DateTimeExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecastApplication.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime ToLocalTime(this long unixTimeStamp)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimeStamp);
+            return dateTimeOffset.LocalDateTime;
+        }
+    }
+}
--- WeatherForecastApplication/Models/WeatherData.cs
+++ WeatherForecastApplication/Models/WeatherData.cs
@@ -1,42 +1,33 @@
-﻿
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
 namespace WeatherForecastApplication.Models
 {
     public class WeatherData
     {
-        public Coord Coord { get; set; }
-        public List<Weather> Weather { get; set; }
-        public string Base { get; set; }
         public Main Main { get; set; }
-        public int Visibility { get; set; }
         public Wind Wind { get; set; }
-        public Clouds Clouds { get; set; }
-        public int Dt { get; set; }
         public Sys Sys { get; set; }
-        public int Id { get; set; }
+        public Rain Rain { get; set; }
         public string Name { get; set; }
-        public int Cod { get; set; }
-    }
-
-    public class Coord
-    {
-        public double Lon { get; set; }
-        public double Lat { get; set; }
-    }
-
-    public class Weather
-    {
-        public int Id { get; set; }
-        public string Main { get; set; }
-        public string Description { get; set; }
-        public string Icon { get; set; }
+        public int Visibility { get; set; }
+        public List<Weather> Weather { get; set; }
     }
 
     public class Main
     {
+        [JsonProperty("temp")]
         public double Temp { get; set; }
+
+        [JsonProperty("feels_like")]
         public double FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
         public double TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
         public double TempMax { get; set; }
+
         public int Pressure { get; set; }
         public int Humidity { get; set; }
     }
@@ -47,17 +38,26 @@ namespace WeatherForecastApplication.Models
         public int Deg { get; set; }
     }
 
-    public class Clouds
-    {
-        public int All { get; set; }
-    }
-
     public class Sys
     {
-        public int Type { get; set; }
-        public int Id { get; set; }
+        public long Sunrise { get; set; }
+        public long Sunset { get; set; }
         public string Country { get; set; }
-        public int Sunrise { get; set; }
-        public int Sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Rain
+    {
+        [JsonProperty("1h")]
+        public double? OneHour { get; set; }
+
+        [JsonProperty("3h")]
+        public double? ThreeHours { get; set; }
     }
 }
